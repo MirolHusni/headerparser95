1740787f750fff27b5e954eada03357113886b4c
Glitch (headerparser95) <none>
2018-03-26 09:06:54 +0000

    🐧😂 Checkpoint ./server.js:762226/295

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+//Used for easy parsing user-agent for response
+const useragent = require('express-useragent');
+
 //Create an instance of express for our app and instantiate bodyParser and Cors
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(useragent.express());
 
 //API URL
 var api = '/api/whoami';
 
 app.get(api, function(request, response, next){
-  console.log('19080');
   
   var language = request.acceptsLanguages();
-  var software = request.get('User-Agent');
+  var software = request.useragent;
   var ipaddress = request.ip;
   
   response.json({
